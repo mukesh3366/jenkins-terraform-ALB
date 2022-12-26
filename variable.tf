9d2fc3e16cdb6349b5edca32f184bb2d02bd89df
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-12-26 22:15:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
-variable "ami"{
- type = string
-  default = "ami-010aff33ed5991201"
-}
-variable "keyname"{
-  default = "MY-TEMP-PVT-INSTANCE"
+variable "ami" {
+  type    = string
+  default = "ami-07ffb2f4d65357b42"
+} 
+variable "keyname" {
+  default = "final"
 }
 variable "region" {
   type        = string
@@ -17,51 +17,51 @@ variable "vpc_cidr" {
   description = "default vpc_cidr_block"
 }
 
-variable "pub_sub1_cidr_block"{
-   type        = string
-   default     = "172.16.1.0/24"
+variable "pub_sub1_cidr_block" {
+  type    = string
+  default = "172.16.1.0/24"
 }
 
-variable "pub_sub2_cidr_block"{
-   type        = string
-   default     = "172.16.2.0/24"
+variable "pub_sub2_cidr_block" {
+  type    = string
+  default = "172.16.2.0/24"
 }
-variable "prv_sub1_cidr_block"{
-   type        = string
-   default     = "172.16.3.0/24"
+variable "prv_sub1_cidr_block" {
+  type    = string
+  default = "172.16.3.0/24"
 }
-variable "prv_sub2_cidr_block"{
-   type        = string
-   default     = "172.16.4.0/24"
-}
-
-
-variable "sg_name"{
- type = string
- default = "alb_sg"
+variable "prv_sub2_cidr_block" {
+  type    = string
+  default = "172.16.4.0/24"
 }
 
-variable "sg_description"{
- type = string
- default = "SG for application load balancer"
+
+variable "sg_name" {
+  type    = string
+  default = "alb_sg"
 }
 
-variable "sg_tagname"{
- type = string
- default = "SG for ALB"
+variable "sg_description" {
+  type    = string
+  default = "SG for application load balancer"
 }
 
-variable "sg_ws_name"{
- type = string
- default = "webserver_sg"
+variable "sg_tagname" {
+  type    = string
+  default = "SG for ALB"
 }
 
-variable "sg_ws_description"{
- type = string
- default = "SG for web server"
+variable "sg_ws_name" {
+  type    = string
+  default = "webserver_sg"
 }
 
-variable "sg_ws_tagname"{
- type = string
- default = "SG for web"
+variable "sg_ws_description" {
+  type    = string
+  default = "SG for web server"
+}
+
+variable "sg_ws_tagname" {
+  type    = string
+  default = "SG for web"
 }
