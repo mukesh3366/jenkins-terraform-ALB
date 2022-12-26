c7dbab040de6ad748a083548b39c8a246e9ff829
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-12-26 22:15:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-   region  = var.region
+  region = var.region
 }
 
 # Create VPC
@@ -9,8 +9,8 @@ resource "aws_vpc" "main" {
 
   tags = {
     Project = "demo-assignment"
-    Name = "My Demo VPC"
- }
+    Name    = "My Demo VPC"
+  }
 }
 
 # Create Public Subnet1
@@ -21,11 +21,9 @@ resource "aws_subnet" "pub_sub1" {
   map_public_ip_on_launch = true
   tags = {
     Project = "demo-assignment"
-     Name = "public_subnet1"
- 
+    Name    = "public_subnet1"
  }
 }
-
 # Create Public Subnet2
 
 resource "aws_subnet" "pub_sub2" {
@@ -48,8 +46,8 @@ resource "aws_subnet" "prv_sub1" {
 
   tags = {
     Project = "demo-assignment"
-    Name = "private_subnet1" 
- }
+    Name    = "private_subnet1"
+  }
 }
 
 # Create Private Subnet2
@@ -72,8 +70,8 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Project = "demo-assignment"
-    Name = "internet gateway" 
- }
+    Name    = "internet gateway"
+  }
 }
 
 # Create Public Route Table
@@ -88,8 +86,8 @@ resource "aws_route_table" "pub_sub1_rt" {
 
   tags = {
     Project = "demo-assignment"
-    Name = "public subnet route table" 
- }
+    Name    = "public subnet route table"
+  }
 }
 
 # Create route table association of public subnet1
@@ -98,15 +96,9 @@ resource "aws_route_table_association" "internet_for_pub_sub1" {
   route_table_id = aws_route_table.pub_sub1_rt.id
   subnet_id      = aws_subnet.pub_sub1.id
 }
-# Create route table association of public subnet2
-
-resource "aws_route_table_association" "internet_for_pub_sub2" {
-  route_table_id = aws_route_table.pub_sub1_rt.id
-  subnet_id      = aws_subnet.pub_sub2.id
-}
 
 # Create EIP for NAT GW1
-  resource "aws_eip" "eip_natgw1" {
+resource "aws_eip" "eip_natgw1" {
   count = "1"
 }
 
@@ -117,7 +109,6 @@ resource "aws_nat_gateway" "natgateway_1" {
   allocation_id = aws_eip.eip_natgw1[count.index].id
   subnet_id     = aws_subnet.pub_sub1.id
 }
-
 # Create EIP for NAT GW2
 
 resource "aws_eip" "eip_natgw2" {
@@ -143,8 +134,8 @@ resource "aws_route_table" "prv_sub1_rt" {
   }
   tags = {
     Project = "demo-assignment"
-    Name = "private subnet1 route table" 
- }
+    Name    = "private subnet1 route table"
+  }
 }
 
 # Create route table association betn prv sub1 & NAT GW1
@@ -179,6 +170,7 @@ resource "aws_route_table_association" "pri_sub2_to_natgw1" {
 }
 
 
+
 # Create security group for load balancer
 
 resource "aws_security_group" "elb_sg" {
@@ -186,7 +178,7 @@ resource "aws_security_group" "elb_sg" {
   description = var.sg_description
   vpc_id      = aws_vpc.main.id
 
-ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -194,18 +186,18 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-egress {
+  egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
- 
- tags = {
-    Name = var.sg_tagname
-    Project = "demo-assignment" 
-  }	
+
+  tags = {
+    Name    = var.sg_tagname
+    Project = "demo-assignment"
+  }
 }
 
 # Create security group for webserver
@@ -215,23 +207,23 @@ resource "aws_security_group" "webserver_sg" {
   description = var.sg_ws_description
   vpc_id      = aws_vpc.main.id
 
-ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     description = "HTTP"
     cidr_blocks = ["0.0.0.0/0"]
 
- }
+  }
 
-ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     description = "HTTP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-egress {
+  egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -239,41 +231,45 @@ egress {
     ipv6_cidr_blocks = ["::/0"]
   }
 
- tags = {
-    Name = var.sg_ws_tagname 
+  tags = {
+    Name    = var.sg_ws_tagname
     Project = "demo-assignment"
   }
+}
+
+data "template_file" "user_data" {
+  template = "${file("userdata.sh")}"
 }
 
 #Create Launch config
 
 resource "aws_launch_configuration" "webserver-launch-config" {
-  name_prefix   = "webserver-launch-config"
-  image_id      =  var.ami
-  instance_type = "t2.micro"
-  key_name	= var.keyname
+  name_prefix     = "webserver-launch-config"
+  image_id        = var.ami
+  instance_type   = "t2.micro"
+  key_name        = var.keyname
   security_groups = ["${aws_security_group.webserver_sg.id}"]
-  
-  root_block_device {
-            volume_type = "gp2"
-            volume_size = 10
-            encrypted   = true
-        }
 
-    ebs_block_device {
-            device_name = "/dev/sdf"
-            volume_type = "gp2"
-            volume_size = 5
-            encrypted   = true
-        }
-       
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 10
+    encrypted   = true
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_type = "gp2"
+    volume_size = 5
+    encrypted   = true
+  }
+
 
   lifecycle {
     create_before_destroy = true
   }
-  user_data = filebase64("${path.module}/init_webserver.sh")
+  #user_data = filebase64("${path.module}/userdata.sh")
+  user_data = "data.template_file.user_data" 
 }
-
 
 # Create Auto Scaling Group
 resource "aws_autoscaling_group" "Demo-ASG-tf" {
@@ -282,7 +278,7 @@ resource "aws_autoscaling_group" "Demo-ASG-tf" {
   max_size           = 2
   min_size           = 1
   force_delete       = true
-  depends_on 	     = ["aws_lb.ALB-tf"]
+  depends_on 	     = [aws_lb.ALB-tf]
   target_group_arns  =  ["${aws_lb_target_group.TG-tf.arn}"]
   health_check_type  = "EC2"
   launch_configuration = aws_launch_configuration.webserver-launch-config.name
@@ -298,18 +294,18 @@ resource "aws_autoscaling_group" "Demo-ASG-tf" {
 # Create Target group
 
 resource "aws_lb_target_group" "TG-tf" {
-  name     = "Demo-TargetGroup-tf"
-  depends_on = ["aws_vpc.main"]
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${aws_vpc.main.id}"
+  name       = "Demo-TargetGroup-tf"
+  depends_on = [aws_vpc.main]
+  port       = 80
+  protocol   = "HTTP"
+  vpc_id     = aws_vpc.main.id
   health_check {
     interval            = 70
     path                = "/index.html"
     port                = 80
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout             = 60 
+    timeout             = 60
     protocol            = "HTTP"
     matcher             = "200,202"
   }
@@ -318,15 +314,15 @@ resource "aws_lb_target_group" "TG-tf" {
 # Create ALB
 
 resource "aws_lb" "ALB-tf" {
-   name              = "Demo-ALG-tf"
+  name               = "Demo-ALG-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.elb_sg.id]
-  subnets            = [aws_subnet.pub_sub1.id,aws_subnet.pub_sub2.id]
+  subnets            = [aws_subnet.pub_sub1.id,aws_subnet.pub_sub2.id] 
 
   tags = {
-	name  = "Demo-AppLoadBalancer-tf"
-    	Project = "demo-assignment"
+    name    = "Demo-AppLoadBalancer-tf"
+    Project = "demo-assignment"
   }
 }
 
