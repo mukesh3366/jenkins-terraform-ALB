ec250000a022f619b25bc2d0e04b7b235e96a13b
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-12-26 22:25:27 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-# Install & Start nginx server
-  yum search nginx 
-  amazon-linux-extras install nginx1 -y
-  systemctl start nginx
-  systemctl enable nginx
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
   
 # Print the hostname which includes instance details on nginx homepage  
-  sudo echo Hello from `hostname -f` > /usr/share/nginx/html/index.html
+#  sudo echo Hello from `hostname -f` > /usr/share/nginx/html/index.html
