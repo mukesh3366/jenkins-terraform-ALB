32ba0f31dcf89999a12504b0147b761eaaf2ad20
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-12-28 22:13:55 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,19 +1,27 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#!/bin/bash
+# Install docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# READ MORE: https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# To check: grep "cloud-init\[" /var/log/syslog
+#       OR: less /var/log/cloud-init-output.log
+
+# Manually add user to docker group
+# sudo usermod -aG docker $USER
 sudo usermod -aG docker ubuntu
-sudo chmod 666 /var/run/docker.sock 
-docker run -d --name mukesh-project -p 80:80 mukesh36/ngnix-app-terraform_docker:19 
+sudo chmod 666 /var/run/docker.sock
+sudo service docker start
+docker run -d --name mukesh-project01 -p 80:80 mukesh36/ngnix-app-terraform_docker:19 
